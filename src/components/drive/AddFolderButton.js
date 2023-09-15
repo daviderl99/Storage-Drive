8ea0563f547f5a080028baa263376fbbae41b17e
daviderl99 <daviderlich75@gmail.com>
2023-09-15 11:48:30 +0300

    Created breadcrumbs and added folder path

--- src/components/drive/AddFolderButton.js
+++ src/components/drive/AddFolderButton.js
@@ -1,14 +1,15 @@
-import React, { useState } from 'react';
-import { Button, Modal, Form } from 'react-bootstrap';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFolderPlus } from '@fortawesome/free-solid-svg-icons';
-import { addDoc } from 'firebase/firestore';
-import { db } from '../../firebase';
-import { useAuth } from '../../contexts/AuthContext';
+import React, { useState } from "react";
+import { Button, Modal, Form } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFolderPlus } from "@fortawesome/free-solid-svg-icons";
+import { addDoc } from "firebase/firestore";
+import { db } from "../../firebase";
+import { useAuth } from "../../contexts/AuthContext";
+import { ROOT_FOLDER } from "../../hooks/useFolder";
 
 export default function AddFolderButton({ currentFolder }) {
   const [open, setOpen] = useState(false);
-  const [name, setName] = useState('');
+  const [name, setName] = useState("");
   const { currentUser } = useAuth();
 
   function openModal() {
@@ -17,7 +18,7 @@ export default function AddFolderButton({ currentFolder }) {
 
   function closeModal() {
     setOpen(false);
-    setName('');
+    setName("");
   }
 
   async function handleSubmit(e) {
@@ -25,21 +26,26 @@ export default function AddFolderButton({ currentFolder }) {
 
     if (currentFolder === null) return;
 
+    const path = [...currentFolder.path];
+    if (currentFolder !== ROOT_FOLDER) {
+      path.push({ name: currentFolder.name, id: currentFolder.id });
+    }
+
     addDoc(db.folders, {
       name: name,
       parentId: currentFolder.id,
       userId: currentUser.uid,
-      // path,
-      createdAt: db.currentTimestamp
+      path: path,
+      createdAt: db.currentTimestamp,
     })
-    .then(() => {
-      console.log(`Created ${name}`);
-    })
-    .catch(error => {
-      console.log("Something went wrong", error);
-    })
-    
-    setName('');
+      .then(() => {
+        console.log(`Created ${name}`);
+      })
+      .catch((error) => {
+        console.log("Something went wrong", error);
+      });
+
+    setName("");
     closeModal();
   }
 
@@ -47,11 +53,11 @@ export default function AddFolderButton({ currentFolder }) {
     <>
       <Button
         onClick={openModal}
-        variant='outline-success'
-        size='sm'
-        className='p-2'
+        variant="outline-success"
+        size="sm"
+        className="p-2 mt-3"
       >
-        <FontAwesomeIcon icon={faFolderPlus} size='xl' />
+        <FontAwesomeIcon icon={faFolderPlus} size="xl" />
       </Button>
       <Modal show={open} onHide={closeModal}>
         <Form onSubmit={handleSubmit}>
@@ -62,16 +68,20 @@ export default function AddFolderButton({ currentFolder }) {
                 type="text"
                 requried
                 value={name}
-                onChange={e => setName(e.target.value)}
+                onChange={(e) => setName(e.target.value)}
               ></Form.Control>
             </Form.Group>
           </Modal.Body>
           <Modal.Footer>
-            <Button variant="secondary" onClick={closeModal}>Close</Button>
-            <Button variant="success" type="submit">Create Folder</Button>
+            <Button variant="secondary" onClick={closeModal}>
+              Close
+            </Button>
+            <Button variant="success" type="submit">
+              Create Folder
+            </Button>
           </Modal.Footer>
         </Form>
       </Modal>
     </>
-  )
+  );
 }
